6105787465b3c9defa71ba80640ff662786da48c
winter000boy <sharmajidurgesh04@gmail.com>
2025-08-25 16:05:19 +0530

    Done Backed

--- src/main/java/com/chat/application/config/WebSocketConfig.java
+++ src/main/java/com/chat/application/config/WebSocketConfig.java
@@ -1,4 +1,28 @@
 package com.chat.application.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // Set Message Broker
+        // expect message with /app/sendmessage
+
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
 }
--- src/main/java/com/chat/application/model/ChatMessage.java
+++ src/main/java/com/chat/application/model/ChatMessage.java
@@ -1,4 +1,15 @@
 package com.chat.application.model;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
 public class ChatMessage {
+
+    private Long id;
+    private String sender;
+    private String content;
 }
